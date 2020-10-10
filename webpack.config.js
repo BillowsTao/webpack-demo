efb82eb8294b079c74467db3e5dad98005a9d14b
Billows Tao <taobillows@163.com>
2020-10-10 17:28:05 +0800

    Add source map, watch mode.

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-cheap-source-map',
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
     new HtmlWebpackPlugin({
       title: '管理输出',
     }),
